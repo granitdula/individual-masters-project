fa26b800b76c28901b605058d78e175b6c64350e
Granit Dula <granitdula98@gmail.com>
2020-12-14 02:36:28 +0000

    feat(skeleton-app): added a skeleton version of the app in main.py (#3)
    Wrote skeleton code for the `Application` class as well as execution of the `run` method in `Application`.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+class Application:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    application = Application()
+    application.run()
