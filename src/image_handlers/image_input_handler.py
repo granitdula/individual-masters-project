5b4fc5f33e6c15a4bdd9daf739435d9cd106d6a2
Granit Dula <granitdula98@gmail.com>
2021-01-16 18:31:03 +0000

    feat(rcnn-segmenter): added implementation of detectron2 for instance segmentation (#5)
    This branch implements instance segmentation on images, for general real world objects, by using an RCNN pre-trained model (more specifically the X101-FPN-3X model found here). from Facebook research's Detectron2 library. The model is used on a temporary image of a living room and visualises the segmentation of the objects, supported by the pre-trained model, in the image.

--- src/image_handlers/image_input_handler.py
+++ src/image_handlers/image_input_handler.py
@@ -1,24 +1,28 @@
 import cv2 as cv
 import sys
 
+from src.image_handlers.rcnn_segmenter import RCNNSegmenter
+
 
 class ImageInputHandler:
 
+    SEGMENT_ACC_THRESHOLD = 0.5
+
     def __init__(self):
-        pass
+        self._rcnn = RCNNSegmenter(self.SEGMENT_ACC_THRESHOLD)
 
     # TODO: Change this to be UI where user can browse for an image.
     def handle_input(self):
-        image = self.__input_image()
+        image = self._input_image()
 
-        cv.imshow("Display window", image)
+        self._rcnn.visualise_segmentation(image)
         _ = cv.waitKey(0)
 
-    def __input_image(self):
-        image_path = sys.path[0] + "/../resources/images/living_room_1.jpg"
+    def _input_image(self):
+        image_path = "./../resources/images/living_room_1_small.jpg"
 
         # TODO: Change image read, to be one that's input by user by browsing files.
-        image = cv.imread(cv.samples.findFile(image_path))
+        image = cv.imread(image_path)
 
         if image is None:
             sys.exit("Could not read the image.")
