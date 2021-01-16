a946a84b54369001b62f244c07c372b0bc35dda2
granitdula <granitdula98@gmail.com>
2021-01-16 18:03:59 +0000

    Refactored image_input_handler.py + removed large living room image

--- src/image_handlers/image_input_handler.py
+++ src/image_handlers/image_input_handler.py
@@ -9,16 +9,16 @@ class ImageInputHandler:
 
     # TODO: Change this to be UI where user can browse for an image.
     def handle_input(self):
-        image = self.__input_image()
+        image = self._input_image()
 
         cv.imshow("Display window", image)
         _ = cv.waitKey(0)
 
-    def __input_image(self):
-        image_path = sys.path[0] + "/../resources/images/living_room_1.jpg"
+    def _input_image(self):
+        image_path = "./../resources/images/living_room_1_small.jpg"
 
         # TODO: Change image read, to be one that's input by user by browsing files.
-        image = cv.imread(cv.samples.findFile(image_path))
+        image = cv.imread(image_path)
 
         if image is None:
             sys.exit("Could not read the image.")
