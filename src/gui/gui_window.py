5e30bcc0f0ae3b3cce2abe62af553c2160ca1543
granitdula <granitdula98@gmail.com>
2021-02-09 03:28:01 +0000

    Added GUI window and widgets + refactored main.py

---
+++ src/gui/gui_window.py
@@ -0,0 +1,60 @@
+import os
+
+from PySide6 import QtWidgets as qtw
+
+from src.image_handlers.image_input_handler import ImageInputHandler
+
+
+BASEDIR = os.path.dirname(__file__)  # Relative reference to resources
+
+
+class GUIWindow(qtw.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self._input_handler = ImageInputHandler()
+
+        self._setup_ui()
+
+        self.show()
+
+    def _setup_ui(self):
+        # Designs the UI of the window including properties and widget components.
+
+        self.setWindowTitle("Photo2Depth")
+
+        self._setup_background()
+        self._setup_gui_widgets()
+
+    def _setup_background(self):
+        self.background_widget = qtw.QWidget(self)
+        self.background_widget.resize(self.width(), self.height())
+        img_path = BASEDIR + "/../../resources/images/gui_images/background_gui.jpeg"
+        background_style = f"background-image: url({img_path}) 0 0 0 0 stretch stretch;"
+        self.background_widget.setStyleSheet(background_style)
+
+    def _setup_gui_widgets(self):
+        self.heading = qtw.QLabel("Photo2Depth")
+        self.heading.setStyleSheet("font-size: 64px; font-family: Helvetica; \
+                                    text-align: center;")
+        self.browse_button = qtw.QPushButton("Browse Button")
+        self.browse_button.clicked.connect(self.on_browse)
+        font = self.browse_button.font()
+        font.setPointSize(16)
+        font.setFamily("Helvetica")
+        self.browse_button.setFont(font)
+
+        self.layout = qtw.QVBoxLayout(self)
+        self.layout.addWidget(self.heading)
+        self.layout.addWidget(self.browse_button)
+
+    def resizeEvent(self, event):
+        self.background_widget.resize(self.width(), self.height())
+
+    def on_browse(self):
+        """
+        Callback function that's called after the browse_button is clicked.
+
+        :return: None.
+        """
+        print("CLICKED!")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-from src.image_handlers.image_input_handler import ImageInputHandler
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from src.gui.gui_window import GUIWindow
+# from src.image_handlers.image_input_handler import ImageInputHandler
 
 
 class Application:
 
     def __init__(self):
-        self._input_handler = ImageInputHandler()
+        self._app = QApplication(sys.argv)
+        self._gui_window = GUIWindow()
+        self._gui_window.resize(800, 600)
+
+        # self._input_handler = ImageInputHandler()
 
     def run(self):
-        self._input_handler.handle_input()
+        # self._input_handler.handle_input()
+        sys.exit(self._app.exec_())
 
 
 if __name__ == '__main__':
